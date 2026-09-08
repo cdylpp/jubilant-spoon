90c5f4773e51152a12445795ea84ae324828b177
cdylpp <cdylpp@yahoo.com>
2026-09-08 09:34:26 -0700

    .

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,8 @@
+import { Card } from '../ui/dashboard/cards';
+import RevenueChart from '../ui/dashboard/revenue-chart';
+import LatestInvoices from '../ui/dashboard/latest-invoices';
+import { lusitana } from '../ui/fonts';
+
 export default function Page() { 
     return <p>Dashboard Page</p>
 }
